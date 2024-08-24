75317ae53f4c2ef6bfcc06b754b391dc9b850bb9
Douglas Martins <douglas5422@outlook.com>
2024-08-24 19:36:30 -0300

    Organizando scripts

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     // Check for an alert
     if (localStorage.getItem('alert')) {
@@ -9,113 +9,82 @@ $(document).ready(function() {
 
     // Remove the spinner from the page when it finishes loading
     $('#spinnerLoadPage').css('display', 'none');
-
-    // Validate the contact form
-    const formContact = document.getElementById('contactForm');
-    if(formContact) {
-        formContact.addEventListener('submit', function(event) {
-            let isValid = true;
-            const fields = document.querySelectorAll('#contactForm input, #contactForm textarea');
-    
-            fields.forEach(function(field) {
-                const value = field.value.trim();
-    
-                if(!value) {
-                    isValid = false;  
-                }
-            });
-    
-            if(!isValid) {
-                $('#contactInvalidMessage').empty();
-                $('#contactInvalidMessage').prepend('<p class= "text-danger">Preencha os campos! </p>');
-    
-                event.preventDefault();
-    
-                setTimeout(function() {
-                    $('#contactInvalidMessage').empty()
-                }, 3000);
-            } else {
-                $('#btn_submit_contact').empty();
-                $('#btn_submit_contact').prepend('Aguarde...');
-            }
-        });
-    }
-
-    // Validate the loginAdmin form
-    const formLoginAdmin = document.getElementById('formLoginAdmin');
-    if(formLoginAdmin) {
-        formLoginAdmin.addEventListener('submit', function(event) {
-            let isValid = true;
-
-            const fields = document.querySelectorAll('#formLoginAdmin input');
-
-            fields.forEach(function(field) {
-                const value = field.value.trim();
-
-                if(!value) {
-                    isValid = false;
-                    console.log(field)
-                    $(field).addClass('field_invalid');
-                }
-            });
-
-            if(!isValid) {
-                event.preventDefault();
-                showAlert('error', 'Preencha todos os campos obrigatórios');
-            } else {
-                $('#btnLoginAdmin').empty();
-                $('#btnLoginAdmin').prepend('Aguarde...');
-            }
-        });
-
-        $('#loginAdmin input').on('input', function() {
-            $(this).removeClass('field_invalid');
-        });
-
-        $('#loginAdmin input').on('blur', function() {
-            const value = $(this).val().trim();
-            if (!value) {
-                $(this).addClass('field_invalid');
-            }
-        });
-    }
-
-    // Validate the login form
-    const formLogin = document.getElementById('formLogin');
-    if(formLogin) {
-        formLogin.addEventListener('submit', function(event) {
-            let isValid = true;
-
-            const fields = document.querySelectorAll('#formLogin input');
-
-            fields.forEach(function(field) {
-                const value = field.value.trim();
-
-                if(!value) {
-                    isValid = false;
-                    console.log(field)
-                    $(field).addClass('field_invalid');
-                }
-            });
-
-            if(!isValid) {
-                event.preventDefault();
-                showAlert('error', 'Preencha todos os campos obrigatórios');
-            } else {
-                $('#btnLoginAdmin').empty();
-                $('#btnLoginAdmin').prepend('Aguarde...');
-            }
-        });
-
-        $('#formLogin input').on('input', function() {
-            $(this).removeClass('field_invalid');
-        });
-
-        $('#formLogin input').on('blur', function() {
-            const value = $(this).val().trim();
-            if (!value) {
-                $(this).addClass('field_invalid');
-            }
-        });
-    }
 });
+
+// Validate the loginAdmin form
+const formLoginAdmin = document.getElementById('formLoginAdmin');
+if (formLoginAdmin) {
+    formLoginAdmin.addEventListener('submit', function (event) {
+        let isValid = true;
+
+        const fields = document.querySelectorAll('#formLoginAdmin input');
+
+        fields.forEach(function (field) {
+            const value = field.value.trim();
+
+            if (!value) {
+                isValid = false;
+                console.log(field)
+                $(field).addClass('field_invalid');
+            }
+        });
+
+        if (!isValid) {
+            event.preventDefault();
+            showAlert('error', 'Preencha todos os campos obrigatórios');
+        } else {
+            $('#btnLoginAdmin').empty();
+            $('#btnLoginAdmin').prepend('Aguarde...');
+        }
+    });
+
+    $('#loginAdmin input').on('input', function () {
+        $(this).removeClass('field_invalid');
+    });
+
+    $('#loginAdmin input').on('blur', function () {
+        const value = $(this).val().trim();
+        if (!value) {
+            $(this).addClass('field_invalid');
+        }
+    });
+}
+
+// Validate the login form
+const formLogin = document.getElementById('formLogin');
+if (formLogin) {
+    formLogin.addEventListener('submit', function (event) {
+        let isValid = true;
+
+        const fields = document.querySelectorAll('#formLogin input');
+
+        fields.forEach(function (field) {
+            const value = field.value.trim();
+
+            if (!value) {
+                isValid = false;
+                console.log(field)
+                $(field).addClass('field_invalid');
+            }
+        });
+
+        if (!isValid) {
+            event.preventDefault();
+            showAlert('error', 'Preencha todos os campos obrigatórios');
+        } else {
+            $('#btnLoginAdmin').empty();
+            $('#btnLoginAdmin').prepend('Aguarde...');
+        }
+    });
+
+    $('#formLogin input').on('input', function () {
+        $(this).removeClass('field_invalid');
+    });
+
+    $('#formLogin input').on('blur', function () {
+        const value = $(this).val().trim();
+        if (!value) {
+            $(this).addClass('field_invalid');
+        }
+    });
+}
