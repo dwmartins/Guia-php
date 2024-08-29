262b0549ab05337ac0e0cf360ae47cf9c0134219
Douglas Martins <douglas5422@outlook.com>
2024-08-28 21:22:23 -0300

    feat: Melhorias

--- src/views/publicView/user/profile.php
+++ src/views/publicView/user/profile.php
@@ -109,35 +109,8 @@
                     <hr class="text-secondary">
                 </div>
 
-                <form action="/user/delete" method="post" class="mt-5">
-                    <!-- Form delete account -->
-                    <p class="fs-5 custom_dark mb-3"><?= DELETE_ACCOUNT ?></p>
-
-                    <div class="alert-danger alert border-0 d-flex flex-nowrap gap-4">
-                        <div>
-                            <i class="fa-solid fa-triangle-exclamation text-danger fs-1"></i>
-                        </div>
-                        <div>
-                            <p class="text-danger fw-semibold mb-0 fs-7"><?= DELETE_ACCOUNT_TEXT ?></p>
-                            <p class="text-danger mb-0 fs-7"><?= ACTION_CANNOT_UNDONE ?></p>
-                        </div>
-                    </div>
-
-                    <label for="confirmAccountDeletion" class="form-check-label text-secondary fs-7 mb-3 cursor_pointer">
-                        <input
-                            type="checkbox"
-                            name="confirmAccountDeletion"
-                            class="form-check-input custom_focus"
-                            id="confirmAccountDeletion"
-                        >
-                        <?= CONFIRM_DELETE_ACCOUNT ?>
-                    </label>
-
-                    <div class="d-flex justify-content-end mt-3">
-                        <button class="btn btn-sm btn-danger"><?= DELETE_ACCOUNT ?></button>
-                    </div>
-
-                </form>
+                <!-- Form delete account -->
+                <?php require ROOT_COMPONENTS . "/public/forms/deleteAccount.php" ?>
 
             </div>
             <div class="col-12 col-lg-5 col-xxl-6 p-3 pt-0 d-none d-lg-flex justify-content-center align-items-baseline">
