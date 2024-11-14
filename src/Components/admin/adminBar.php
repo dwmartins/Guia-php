d01b766dfa09b42ce0eaae4957a8e3879212c81f
Douglas Martins <douglas5422@outlook.com>
2024-11-14 20:13:20 -0300

    fix: Correção na tradução da adminBar

--- src/Components/admin/adminBar.php
+++ src/Components/admin/adminBar.php
@@ -9,13 +9,13 @@ if (isAdmin()): ?>
 
             <?php if (MAINTENANCE): ?>
                 <div class="item_center gap-2 publish">
-                    <p class="m-0 text-white-50 fs-7">Site em manutenção"</p>
-                    <a href="/app/configurações/configurações-gerais" class="btn btn-sm btn-outline-primary">Publicar</a>
+                    <p class="m-0 text-white-50 fs-7"><?= MAINTENANCE_ALERT ?></p>
+                    <a href="/app/configurações/configurações-gerais" class="btn btn-sm btn-outline-primary"><?= PUBLISH ?></a>
                 </div>
             <?php endif ?>
 
             <a href="/logout" class="text-white-50 outline_none hover_primary">
-                <span class="d-none d-sm-inline-block">Sair</span>
+                <span class="d-none d-sm-inline-block"><?= LOGOUT_PAGE ?></span>
                 <i class="fa-solid fa-right-from-bracket ms-1"></i>
             </a>
         </div>
